772f0707cbcaf9c103ec9cca1c25362ccd0a2c7e
Piguave <ariel_97_xd@hotmail.com>
2022-11-25 23:34:04 -0500

    Componente Home terminado, arreglando detalles con el repositorio en githug

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+
+function Home() {
+    return (
+      <>
+      <div className="home" >
+              <h3 className="titulohome">Welcome to my Playground</h3>
+              <p className="descriptionhome">Hi Everyone!, my name is Angel Ariel Piguave Bermemllo and this is my playground, check some of my projects. If you like what you see don’t hesitate to contact me, I’ll be glad to help you</p>
+      </div>
+      </>
+    );
+  }
+  
+  export default Home;
